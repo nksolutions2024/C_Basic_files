301c7788cb777a45bd002eb2d28cbfb7721d0685
Sandesh <sdsoni2042@gmail.com>
2024-07-03 16:20:24 +0530

    added file4

---
+++ file_4.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void main(){
+    printf("%d",sizeof(int));
+    printf("\n%d",sizeof(float));
+    printf("\n%d",sizeof(char));
+
+    printf("\n\n%d",sizeof(double));
+    printf("\n%d",sizeof(long));
+    printf("\n%d",sizeof(long int));
+    printf("\n%d",sizeof(long long int));
+}
+//output
+// 4
+// 4
+// 1
+
+// 8
+// 4
+// 4
+// 8
